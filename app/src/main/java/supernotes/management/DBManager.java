d9bd324902164867c0d0ef3eecc1945c923d6311
dounyaa <dounyaa.alaoui@gmail.com>
2023-12-10 11:39:25 +0100

    Integrate NotionAPI into management functionalities

--- app/src/main/java/supernotes/management/DBManager.java
+++ app/src/main/java/supernotes/management/DBManager.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 
 public interface DBManager {
     void createNotesTable();
-    void addTextNote(String content, String tag);
-    void addImageNote(byte[] imageBytes, String tag);
+    void addTextNote(String title, String content, String tag, String parent_page_id, String page_id);
+    void addImageNote(String title, byte[] imageBytes, String tag, String parent_page_id, String page_id);
     void deleteNoteByTag(String tag);
     ArrayList<Note> getAllNotes();
     ArrayList<Note> getAllNotesByTag(String tag);
     ArrayList<Note> getAllNotesLike(String contentMotif);
+    String getParentPageId();
+    String getPageId(String Content);
+    void updateNoteContentInDB(String pageId, String newContent);
+    boolean doesNoteExist(String pageId);
 }
--- app/src/main/java/supernotes/management/NoteManagerDataBase.java
+++ app/src/main/java/supernotes/management/NoteManagerDataBase.java
@@ -14,14 +14,22 @@ public class NoteManagerDataBase implements NoteManager {
     @Override
     public void addNote(Note note) {
         if (note instanceof TextNote){
+            String title = note.getTitle();
             String noteContent = ((TextNote) note).getContent();
             String noteTag = note.getTag();
-            dbManager.addTextNote(noteContent, noteTag);
+            String parent_page_id = note.getParentPageId();            
+            String page_id = note.getPageId();
+
+            dbManager.addTextNote(title, noteContent, noteTag, parent_page_id, page_id);
         }
         else if (note instanceof ImageNote){
+            String title = note.getTitle();
             var imageContent = ((ImageNote) note).getContent();
             String noteTag = note.getTag();
-            dbManager.addImageNote(imageContent, noteTag);
+            String parent_page_id = note.getParentPageId();     
+            String page_id = note.getPageId();
+
+            dbManager.addImageNote(title, imageContent, noteTag, parent_page_id, page_id);
         }
     }
 
@@ -30,4 +38,29 @@ public class NoteManagerDataBase implements NoteManager {
     {
         dbManager.deleteNoteByTag(tag);
     }
+
+    @Override
+    public String getParentPageId()
+    {
+        return dbManager.getParentPageId();
+    }
+
+    @Override
+    public String getPageId(String Content)
+    {
+        return dbManager.getPageId(Content);
+    }
+    
+    @Override
+    public void updateNoteContentInDB(String pageId, String newContent)
+    {
+        dbManager.updateNoteContentInDB(pageId, newContent);
+    }
+
+    @Override
+    public boolean doesNoteExist(String pageId)
+    {
+        return dbManager.doesNoteExist(pageId);
+    }
+    
 }
