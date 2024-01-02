ea28a17fa8dae86b166f4369ba40848ff5da1ac2
dounyaa <78869241+dounyaa@users.noreply.github.com>
2024-01-02 21:33:40 +0100

    Update DBManager.java

--- app/src/main/java/supernotes/management/DBManager.java
+++ app/src/main/java/supernotes/management/DBManager.java
@@ -7,17 +7,13 @@ import java.util.List;
 
 public interface DBManager {
     void createNotesTable();
-    void addTextNote(String title, String content, String tag, String parent_page_id, String page_id);
-    void addTextNote(String title, String type, String content, String tag, String parent_page_id, String page_id, String time);
-    void addImageNote(String title, byte[] imageBytes, String tag, String parent_page_id, String page_id);
-    void addImageNote(String title, String type, byte[] imageBytes, String tag, String parent_page_id, String page_id, String time, String path);
+    int addTextNote(String title, String content, String tag, String parent_page_id, String page_id, String time);
+    int addImageNote(String title, byte[] imageBytes, String tag, String parent_page_id, String page_id, String time, String path);
     void createRemindersTable();
     void addReminder(int noteId, LocalDateTime reminderDateTime);
     List<LocalDateTime> getReminders(int noteId);
     boolean deleteRemindersByNoteId(int noteId);
 
-    int addTextNote(String title, String content, String tag, String parent_page_id, String page_id);
-    int addImageNote(String title, byte[] imageBytes, String tag, String parent_page_id, String page_id);
     void deleteNoteByTag(String tag);
 
     ArrayList<Note> getAllNotes();
